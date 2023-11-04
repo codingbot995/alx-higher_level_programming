6317d1ff0ff32786a0845d8b08dc06fba405aa77
Jane mathenge <waguthijane61@gmail.com>
2023-11-04 00:37:42 -0700

    c-removed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for element in my_string:
         if element != "c" and element != "C":
             new_string += element
-            return new_string
+    return new_string
