86d41d7841596ee0fbea6c0c5f34b9a68cd11053
Jane mathenge <waguthijane61@gmail.com>
2023-11-04 00:33:23 -0700

    string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for element in my_string:
-        if element != c and element != C:
+        if element != "c" and element != "C":
             new_string += element
             return new_string
