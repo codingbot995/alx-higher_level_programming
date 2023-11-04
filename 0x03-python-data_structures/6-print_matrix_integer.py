bc589ac443c524d35f40408a326a22b1067d3fac
Jane mathenge <waguthijane61@gmail.com>
2023-11-04 00:48:45 -0700

    copy

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         for row in range(len(matrix)):
             for item in range(len(matrix[row])):
                  if item != len(matrix[row]) - 1:
-                    endspace = ' '
+                    endspace =''
                 else:
                     endspace = ''
                 print("{:d}".format(matrix[row][item]), end=endspace)
