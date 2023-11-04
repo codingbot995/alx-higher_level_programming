7c85cc40fbac21eaff149c58ab8de48f340d1877
Jane mathenge <waguthijane61@gmail.com>
2023-11-04 00:24:05 -0700

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
+    len_ = len(my_list)
+
+    if idx < 0 or idx > len_:
         return my_list
-    if idx >= len(my_list):
+    else:
+        my_list.insert(idx, element)
         return my_list
-    my_list[idx] = element
-    return my_list
